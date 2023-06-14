09a03eb8a6bf99402bd77188831de6d90356dd0b
MsEmily1020 <peace10200khs@gmail.com>
2023-06-14 11:09:06 +0900

    [Feat] Search Button 클릭했을 경우 LoadingText 띄우기
    - loading TextView를 만들어 id값 주기
    - 처음에는 loadingTextView를 Gone으로 안보이게 하기
    - SearchButton을 클릭했을 경우 View.VISIBLE로 보이게 하기
    
    Ref: #3

--- app/src/main/java/com/example/githubsearch/MainActivity.kt
+++ app/src/main/java/com/example/githubsearch/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.githubsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        val loadingText = findViewById<TextView>(R.id.loading_text)
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+
+        searchBtn.setOnClickListener {
+            loadingText.visibility = View.VISIBLE
+        }
     }
 }
