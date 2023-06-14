d1e26892d3f31c15c01fee26e1c103415333ef7f
MsEmily1020 <peace10200khs@gmail.com>
2023-06-14 11:45:06 +0900

    [Feat] Retrofit.Builder() 생성
    MainActivity에 작성
    - api.github.com 정보 불러오기 위해 Retrofit 사용
    - build.gradle(Module) 및 Manifest 파일 설정 추가
    
    Ref: #4

--- app/src/main/java/com/example/githubsearch/MainActivity.kt
+++ app/src/main/java/com/example/githubsearch/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
         val loadingText = findViewById<TextView>(R.id.loading_text)
+        val searchText = findViewById<EditText>(R.id.search_text)
         val searchBtn = findViewById<Button>(R.id.search_btn)
 
         searchBtn.setOnClickListener {
             loadingText.visibility = View.VISIBLE
+
+            val username = searchText.text.trim().toString().replace(" ", "")
         }
     }
 }
